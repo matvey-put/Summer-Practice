b718f46938ee8f2f7fbbcab52d9deb804f168616
matvey-put <matveyhuskin0923@gmail.com>
2024-07-03 21:30:16 +0600

    feat: Backgraund and Logo

--- app/src/main/java/com/example/summerpractice/MainActivity.kt
+++ app/src/main/java/com/example/summerpractice/MainActivity.kt
@@ -4,13 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Card
+
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import com.example.summerpractice.ui.theme.SummerPracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +48,51 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SummerPracticeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Backgraund()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Backgraund() {
+    Box(Modifier.background(Color.Black))
+    {
+        Image(
+            painter = painterResource(R.drawable.back),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(vertical = 20.dp)
+                .fillMaxSize(1f)
+
+        )
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = painterResource(R.drawable.logo),
+                contentDescription = null,
+                alignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(vertical = 10.dp, horizontal = 120.dp)
+                    .width(150.dp)
+                    .height(100.dp)
+
+            )
+            Text(
+                text = "Choose your hero",
+                style = TextStyle(
+                    fontSize = 30.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    SummerPracticeTheme {
-        Greeting("Android")
-    }
+    Backgraund()
+
 }
